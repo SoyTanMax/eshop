a341298a85a420893a2c49eb8cf3a5bbc8c213d8
Max López <45810804+SoyTanMax@users.noreply.github.com>
2022-05-06 11:48:20 -0500

    styles

--- src/components/SingleOrder.vue
+++ src/components/SingleOrder.vue
@@ -7,7 +7,12 @@
             </div>
             <div class="bottom-info is-flex">
                 <p class="is-size-7">{{ Date(order.dates.updatedAt).slice(4,16) }}</p>
-                <p class="is-capitalized">{{order.payment.status}}</p>
+                <div class="is-capitalized tag">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                    </svg>
+                    {{order.payment.status}}
+                </div>
             </div>
         </router-link>
     </div>
@@ -41,5 +46,13 @@
 .top-info, .bottom-info{
     justify-content: space-between;
     align-items: center;
+}
+.tag{
+    background: #DAFFDB;
+    color: #15A936;
+}
+svg{
+    width: 16px;
+    margin-right: 8px;
 }
 </style>
